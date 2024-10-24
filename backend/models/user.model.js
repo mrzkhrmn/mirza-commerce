973b3da11385e93393fa7614e16b0e84bfda44f6
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-10-24 03:23:09 +0300

    creating user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
