34a61c467ecea85b3eb8341234e7d7d7620c191a
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-10-24 03:15:46 +0300

    setting up our auth routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+export const signup = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import authRoutes from "./routes/auth.routes.js";
+
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -11,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
